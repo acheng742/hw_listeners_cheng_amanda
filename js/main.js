ebc9dfe2196fc35f6ebcc9545ed933aa6568443b
Amanda <acheng742@gmail.com>
2017-03-24 19:17:13 -0400

    add second js function for random # event listener

--- js/main.js
+++ js/main.js
@@ -5,3 +5,8 @@ var heading = document.getElementById("h1");
 heading.addEventListener("click", function (){
 	heading.className = "yellow";
 }); //this event listens to a click in the heading and changes the color to yellow
+
+
+window.addEventListener("resize", function(){
+    document.getElementById("randomizer").innerHTML = Math.random();
+}); //this event listens to the resizing of the browser and displays random numbers when resized
