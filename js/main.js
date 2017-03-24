514031e88408d97d2247d9db211d0a094527c8ad
Amanda <acheng742@gmail.com>
2017-03-24 19:40:17 -0400

    add third js function to display img for mouseover

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ heading.addEventListener("click", function (){
 window.addEventListener("resize", function(){
     document.getElementById("randomizer").innerHTML = Math.random();
 }); //this event listens to the resizing of the browser and displays random numbers when resized
+
+
+var image = document.getElementById("kimchi");
+image.addEventListener("mouseover", myFunction);
+
+function myFunction() {
+    document.getElementById("mouseover").innerHTML = "Isn't she just so cute??";
+} // this event listens for the mouse moving over the image to display text
