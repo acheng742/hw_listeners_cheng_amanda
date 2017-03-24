23fe8fdaeed4d195c0f907c34267d7621c2121d2
Amanda <acheng742@gmail.com>
2017-03-24 17:36:38 -0400

    edit misplaced quotation marks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Listener branch
 
-var header = document.getElementById('h1');
+var header = document.getElementById("h1");
 var i = 0;
 
-header.addEventListener('click', function () {
-    var paragraph = document.createElement('paragraph');
-    paragraph.innerHTML = '<p>This is click number</p>' + (++i);
-    document.getElementById('h1').appendChild(paragraph);
+header.addEventListener("click", function () {
+    var paragraph = document.createElement("paragraph");
+    paragraph.innerHTML = "<p>This is click number</p> " + (++i);
+    document.getElementById("h1").appendChild(paragraph);
 });
