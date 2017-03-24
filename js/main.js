bdb46b326b42eca2b2f8ec6546a96445af6764f7
Amanda <acheng742@gmail.com>
2017-03-24 18:39:37 -0400

    add content, styling & add heading event listener

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Custom branch
+
+var heading = document.getElementById("h1");
+
+heading.addEventListener("click", function (){
+	heading.className = "yellow";
+}); //this event listens to a click in the heading and changes the color to yellow
